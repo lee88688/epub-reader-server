85fa5a128d0adb04547639f77bc744a3854e99c0
lee <lee2py@gmail.com>
2020-08-19 17:43:59 +0800

    fix create asar file bug and add more detail test

--- app/service/file.js
+++ app/service/file.js
@@ -4,10 +4,10 @@ const { Service } = require('egg');
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
-const unzip = require('unzip');
-const fstream = require('fstream');
+const unzipper = require('unzipper');
 const asar = require('asar');
 const { fsGetter, EasyAsar } = require('asar-async');
+const { index: asarIndexSymbol } = require('asar-async/dist/base');
 
 class FileService extends Service {
   async convertEpubToAsar(fileStream, destFileName) {
@@ -16,22 +16,51 @@ class FileService extends Service {
     if (!fs.existsSync(outputDir)) {
       await mkdirp(outputDir);
     }
-    // const writeStream = fstream.Writer(outputDir);
-    fileStream
-      .pipe(unzip.Parse())
+
+    const writeProcess = [];
+    await fileStream
+      .pipe(unzipper.Parse({ verbose: false }))
       .on('entry', entry => {
         const { type, path: fileName } = entry;
-        console.log(fileName);
-        if (type === 'Directory' || fileName.endsWith('/')) {
-          mkdirp(path.join(outputDir, fileName));
+        if (type === 'Directory') {
+          writeProcess.push(mkdirp(path.join(outputDir, fileName)));
         } else {
-          entry.pipe(fs.createWriteStream(path.join(outputDir, fileName)))
+          writeProcess.push(new Promise(async (resolve, reject) => {
+            const fileNameDir = path.join(outputDir, fileName);
+            const parentDir = path.dirname(fileNameDir);
+            // make sure the parent dir exists
+            if (!fs.existsSync(parentDir)) {
+              await mkdirp(parentDir);
+            }
+            entry
+              .pipe(fs.createWriteStream(fileNameDir))
+              .on('close', resolve)
+              .on('error', reject);
+          }));
         }
-      });
-    await new Promise((resolve, reject) => {
-      fileStream.on('end', resolve);
-      fileStream.on('error', reject);
-    });
+      })
+      .promise();
+    await Promise.all(writeProcess);
+
+    // waiting in async iterater body may lost unzip data
+    // const entryProcess = fileStream
+    //   .pipe(unzipper.Parse({ verbose: false, forceStream: true }));
+    // for await (const entry of entryProcess) {
+    //   const { type, path: fileName } = entry;
+    //   console.log(fileName);
+    //   if (type === 'Directory') {
+    //     writeProcess.push(mkdirp(path.join(outputDir, fileName)));
+    //   } else {
+    //     writeProcess.push(new Promise((resolve, reject) => {
+    //       entry
+    //         .pipe(fs.createWriteStream(path.join(outputDir, fileName)))
+    //         .on('close', resolve)
+    //         .on('error', reject);
+    //     }));
+    //   }
+    // }
+    // await Promise.all(writeProcess);
+
     const asarDest = path.join(app.config.asarDir, `${destFileName}.asar`);
     if (!fs.existsSync(app.config.asarDir)) {
       await mkdirp(app.config.asarDir);
@@ -44,6 +73,12 @@ class FileService extends Service {
     await ar.fetchIndex();
     const buffer = await ar.readFile(filePath);
     return buffer;
+  }
+
+  async readAsarIndex(asarFile) {
+    const ar = new EasyAsar(fsGetter(asarFile));
+    await ar.fetchIndex();
+    return ar[asarIndexSymbol];
   }
 }
 
--- test/app/service/file.test.js
+++ test/app/service/file.test.js
@@ -2,26 +2,74 @@
 
 const { app, assert } = require('egg-mock/bootstrap');
 const fs = require('fs');
+const fsPromises = fs.promises;
 const path = require('path');
-// const xml2js = require('xml2js');
+const rimraf = require('rimraf');
+const xml2js = require('xml2js');
 
 describe('file service test', () => {
   const fileName = 'sample1';
+  let asarDir = '';
+
+  before(() => {
+    asarDir = path.join(app.config.asarDir, `${fileName}.asar`);
+  });
 
   it('convert epub to asar', async () => {
     const ctx = app.mockContext();
     const epubDir = path.join(app.config.baseDir, 'test/assets/sample1.epub');
     const readStream = fs.createReadStream(epubDir);
+    readStream.fileName = fileName;
     await ctx.service.file.convertEpubToAsar(readStream, fileName);
-    const asarDir = path.join(app.config.asarDir, `${fileName}.asar`);
     assert(fs.existsSync(asarDir));
   });
 
-  // it('read asar content file', async () => {
-  //   const ctx = app.mockContext();
-  //   const asarDir = path.join(app.config.asarDir, `${fileName}.asar`);
-  //   const buffer = await ctx.service.file.readAsarFile(asarDir, 'META-INF/container.xml');
-  //   const xmlObj = await xml2js.parseStringPromise(buffer.toString('utf8'));
-  //   assert(xmlObj.container.rootfiles[0].rootfile[0].$['full-path'] === 'content.opf');
-  // });
+  it("compare directory structure and files' size", async () => {
+    const ctx = app.mockContext();
+    const index = await ctx.service.file.readAsarIndex(asarDir);
+    // console.log(JSON.stringify(index));
+    const topDir = path.join(app.config.tempDir, fileName);
+    const queue = [[ topDir, index ]];
+    let next;
+    // eslint-disable-next-line no-cond-assign
+    while ((next = queue.shift())) {
+      const [ dir, idx ] = next;
+      const dirList = await fsPromises.readdir(dir);
+      if (dirList.length !== Object.keys(idx.files).length) throw new Error(`path(${dir}) is not the same as asar`);
+      await Promise.all(dirList.map(async d => {
+        const currentPath = path.join(dir, d);
+        if (!idx.files[d]) throw new Error(`path(${currentPath}) is not in asar`);
+        if ('files' in idx.files[d]) {
+          // current d is a directory
+          queue.push([ currentPath, idx.files[d] ]);
+          return;
+        }
+        const stat = await fsPromises.stat(currentPath);
+        if (stat.size !== idx.files[d].size) {
+          // file size is not the same as asar.
+          throw new Error(`file(${currentPath}) size is not the same in asar. file(${stat.size}) vs asar(${idx.files[d].size})`);
+        }
+      }));
+    }
+  });
+
+  it('read asar content file', async () => {
+    // todo: add file content compare
+    const ctx = app.mockContext();
+    const buffer = await ctx.service.file.readAsarFile(asarDir, 'META-INF/container.xml');
+    const xmlObj = await xml2js.parseStringPromise(buffer.toString('utf8'));
+    assert(xmlObj.container.rootfiles[0].rootfile[0].$['full-path'] === 'content.opf');
+    await ctx.service.file.readAsarFile(asarDir, 'toc.ncx');
+    await ctx.service.file.readAsarFile(asarDir, 'OEBPS/cover.xml');
+  });
+
+  after(async () => {
+    await new Promise(resolve => {
+      const outputDir = path.join(app.config.tempDir, fileName);
+      rimraf(outputDir, resolve);
+    });
+    await new Promise(resolve => {
+      rimraf(asarDir, resolve);
+    });
+  });
 });
