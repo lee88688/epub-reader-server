3370d28c89ce9de94a702786d050c17c0c0d3d15
lee <lee2py@gmail.com>
2020-11-19 22:06:36 +0800

    add user add and remove script

---
+++ scripts/user.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const getUserModel = require('../app/model/user');
+
+const argv = yargs(hideBin(process.argv))
+  .command('add', 'add a user with email and password', yargs => {
+    yargs
+      .option('name', {
+        alias: 'n',
+        describe: 'user name',
+      })
+      .option('password', {
+        alias: 'p',
+        describe: 'user password',
+
+      })
+      .option('email', {
+        alias: 'e',
+        describe: 'user email',
+      })
+      .demandOption([ 'email', 'password' ], 'email and password must be provided.');
+  })
+  .command('remove', 'remove a user with email', yargs => {
+    yargs
+      .option('email', {
+        alias: 'e',
+        describe: 'user email',
+      })
+      .demandOption('email', 'email is need.');
+  })
+  .help()
+  .demandCommand()
+  .argv;
+
+
+mongoose.connect(process.env.MONGO_DB);
+// mongoose.connect('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const UserModel = getUserModel({ mongoose });
+
+const getArgvField = (field, index) => (Array.isArray(field) ? field[index] : field);
+
+(async () => {
+  const promises = argv._.map(async (command, index) => {
+    if (command === 'add') {
+      const email = getArgvField(argv.email, index);
+      const password = getArgvField(argv.password, index);
+      const name = getArgvField(argv.name, index);
+      const user = new UserModel({ name, email, password });
+      await user.save();
+    } else if (command === 'remove') {
+      const email = getArgvField(argv.email, index);
+      await UserModel.deleteOne({ email });
+    }
+  });
+  await Promise.all(promises);
+  mongoose.disconnect();
+})();
